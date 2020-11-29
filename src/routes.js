706f48019389b3ba483fd2f3ea947f9bda3b2ed5
lucas-mateus <xlucasmat@gmail.com>
2020-11-29 05:07:56 -0300

    adicionando rotas

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,22 @@ const express = require('express');
 const multer = require('multer')
 
 const uploadConfig = require('./config/upload');
+const ApprovalController = require('./controllers/ApprovalController');
 const EventController = require('./controllers/EventController');
 const LoginController = require('./controllers/LoginController');
+const RegistrationController = require('./controllers/RegistrationController');
+const RejectionController = require('./controllers/RejectionController');
 const UserController = require('./controllers/UserController');
 
 const routes = express.Router();
 const upload = multer(uploadConfig);
+
+
+//Registration
+routes.post('/registration/:eventId', RegistrationController.create);
+routes.get('/registration/:regId', RegistrationController.getRegistration);
+routes.post('/registration/:regId/approvals', ApprovalController.approval);
+routes.post('/registration/:regId/rejections', RejectionController.rejection);
 
 //login routes
 routes.post('/login', LoginController.store)
@@ -17,7 +27,7 @@ routes.get('/events', EventController.getAllEvents);
 routes.get('/event/:eventId', EventController.getEventById);
 routes.get('/events/:category', EventController.getByCategory);
 routes.post('/event/register', upload.single("thumbnail"), EventController.save);
-routes.delete('/event/:eventId',  EventController.delete);
+routes.delete('/event/:eventId', EventController.delete);
 
 //user routes
 routes.post('/user/register', UserController.save);
