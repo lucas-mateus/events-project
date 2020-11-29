cbb6853d4028cf36709d173d7112afddd59380a0
lucas-mateus <xlucasmat@gmail.com>
2020-11-29 03:59:50 -0300

    implementando rotas

--- src/routes.js
+++ src/routes.js
@@ -3,14 +3,21 @@ const multer = require('multer')
 
 const uploadConfig = require('./config/upload');
 const EventController = require('./controllers/EventController');
+const LoginController = require('./controllers/LoginController');
 const UserController = require('./controllers/UserController');
 
 const routes = express.Router();
 const upload = multer(uploadConfig);
 
+//login routes
+routes.post('/login', LoginController.store)
+
 //events routes
-routes.get('/event/:eventId', EventController.getEventById)
+routes.get('/events', EventController.getAllEvents);
+routes.get('/event/:eventId', EventController.getEventById);
+routes.get('/events/:category', EventController.getByCategory);
 routes.post('/event/register', upload.single("thumbnail"), EventController.save);
+routes.delete('/event/:eventId',  EventController.delete);
 
 //user routes
 routes.post('/user/register', UserController.save);
