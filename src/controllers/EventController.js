cdf18bad05464ff1365a274fef5495779bbe9193
lucas-mateus <xlucasmat@gmail.com>
2020-11-29 03:58:42 -0300

    implementando os outros métodos do controller

--- src/controllers/EventController.js
+++ src/controllers/EventController.js
@@ -4,7 +4,7 @@ const User = require("../models/User");
 module.exports = {
   
     async save(req,res){
-        const {title, description, price} = req.body;
+        const {title, description, price,category} = req.body;
         const {filename} = req.file;
         const {user_id} = req.headers;
 
@@ -21,7 +21,8 @@ module.exports = {
             description,
             price:parseFloat(price),
             user:user_id,
-            thumbnail:filename
+            thumbnail:filename,
+            category
         })
 
         return res.json(event);
@@ -39,6 +40,55 @@ module.exports = {
          
             
         } catch (error) {
+            return res.status(400).json({
+                message:"Não foi possível encontrar esse evento."
+            })
+        }
+    },
+
+    async getByCategory(req, res){
+        const {category} = req.params;
+
+        try{
+            const event = await Event.find({category});
+            if(event.length>0){
+                return res.json(event);
+            }else{
+                return res.status(400).json({
+                    message: "Não foi encontrado evento com essa categoria"
+                })
+            }
+        }catch(error){
+            console.log(error)
+            return res.status(400).json({
+                message: "Algo deu errado :/"
+            })
+        }
+    },
+
+    async getAllEvents(req, res){
+        try{
+            const events = await Event.find({});
+            
+            if(events!=null){
+                return res.json(events);
+            }
+
+        }catch(error){
+            return res.status(400).json({
+                message:"Algo deu errado... :/"
+            })
+        }
+    },
+
+    async delete(req, res){
+        const {eventId} = req.params;
+        try{
+            await Event.findByIdAndDelete(eventId);
+            //console.log({eventId})
+            return res.status(204).send();
+
+        }catch(error){
             return res.status(400).json({
                 message:"Não foi possível encontrar esse evento."
             })
