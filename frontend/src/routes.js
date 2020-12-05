9e4d8860d7f978fe76a2a4d41cf88678c48af395
lucas-mateus <xlucasmat@gmail.com>
2020-12-05 01:58:48 -0300

    configurando as rotas da aplicação no front

---
+++ frontend/src/routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import Register from './pages/Register';
+
+export default function Routes(){
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={Login}/>
+                <Route path="/register/user" exact component={Register}/>
+                <Route path="/dashboard" component={Dashboard}/>
+            </Switch>
+        </BrowserRouter>
+    )
+}
